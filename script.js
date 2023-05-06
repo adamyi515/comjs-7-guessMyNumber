97e30ab0a432e460d8ec30693e5ad03008019849
Adam Yi <adamyi515@gmail.com>
2023-05-06 18:33:20 -0500

    Add comments to script. Add styles to the page when user wins.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 'use strict';
 const gameNumber = getNumber();
+console.log(gameNumber);
 let score = 20;
 
 
 // Event Handlers
 document.querySelector('.check').addEventListener('click', function(){
-    const userGuess = document.querySelector('.guess').value;
-    console.log(userGuess);
+    const userGuess = parseInt(document.querySelector('.guess').value);
 
     // If user does NOT enter any value.
     if(!userGuess){
@@ -14,14 +14,24 @@ document.querySelector('.check').addEventListener('click', function(){
     } else if(userGuess === gameNumber){
         // If user matches the number, you win.
         document.querySelector('.message').textContent = 'You win!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+    // Guess is too high
     } else if(userGuess > gameNumber){
-        document.querySelector('.message').textContent = 'Too high!';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too high!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+
+    // Guess is too low
     } else if(userGuess < gameNumber){
-        document.querySelector('.message').textContent = 'Too low!';
-        score--;
-        document.querySelector('.score').textContent = score;
+        if(score > 1){
+            document.querySelector('.message').textContent = 'Too low!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
     }
 });
 
