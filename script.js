c80182c49d3dc5dbe99649ee1fb80e19b446d939
Adam Yi <adamyi515@gmail.com>
2023-05-08 15:38:15 -0500

    Create function for displaying message. Use function to update message.

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ document.querySelector('.check').addEventListener('click', function(){
 
     // If user does NOT enter any value.
     if(!userGuess){
-        document.querySelector('.message').textContent = 'Please type a number.';
+        displayMessage('Please type a number');
     } else if(userGuess === gameNumber){
         // If user matches the number, you win.
-        document.querySelector('.message').textContent = 'You win!';
+        displayMessage('You win!');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').textContent = gameNumber;
         document.querySelector('.number').style.width = '30rem';
@@ -27,42 +27,18 @@ document.querySelector('.check').addEventListener('click', function(){
             document.querySelector('.highscore').textContent = highscore;
         }
 
-    // Guess is too high
+    // Guess is too high or too low.
     } else if(userGuess !== gameNumber){
         if(score > 1){
-            document.querySelector('.message').textContent = userGuess > gameNumber ?
-                'Too high!' : 'Too low!';
+            displayMessage(userGuess > gameNumber ? 'Too high!' : 'Too low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'You lose the game!';
+            displayMessage('You lose the game!');
             score = 0;
             document.querySelector('.score').textContent = score;
         }
     }
-
-    
-    // else if(userGuess > gameNumber){
-    //     if(score > 1){
-    //         document.querySelector('.message').textContent = 'Too high!';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You lose the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-
-    // // Guess is too low
-    // } else if(userGuess < gameNumber){
-    //     if(score > 1){
-    //         document.querySelector('.message').textContent = 'Too low!';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = 'You lose the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-    // }
 });
 
 
@@ -82,6 +58,10 @@ function getNumber(){
     return (Math.floor(Math.random() * 20) + 1);
 }
 
+function displayMessage(msg){
+    document.querySelector('.message').textContent = msg;
+}
+
 function init(){
     gameNumber = getNumber();
     console.log(gameNumber);
@@ -95,7 +75,6 @@ function init(){
     document.querySelector('.number').style.width = '15rem';
 
     // Revert the game message back to the original
-    document.querySelector('.message').textContent = 'Start guessing...';
-
+    displayMessage('Start guessing...');
 
 }
